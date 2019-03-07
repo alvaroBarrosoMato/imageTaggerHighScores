fe443b29aa5cf2528887790c80ffab49fbdb5cb1
Alvaro Barroso <abarroso@salesforce.com>
2019-03-07 12:41:56 +0100

    no message

--- src/main/java/ImageTaggerHighScores/ImageTaggerHighScores/Controller.java
+++ src/main/java/ImageTaggerHighScores/ImageTaggerHighScores/Controller.java
@@ -65,7 +65,6 @@ public class Controller {
             out.close(); 
             file.close(); 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 	}
